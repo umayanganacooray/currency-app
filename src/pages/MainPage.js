ee61cd5f0f7c5dbd619353dc73b6c3ac40572498
umayanaganacooray <ume.cooray@gmail.com>
2024-05-05 07:27:24 +0530

    Adding currency-app

---
+++ src/pages/MainPage.js
@@ -0,0 +1,11 @@
+ import React from 'react'
+ 
+ export default function MainPage() {
+   return (
+     <div>
+        <h1 className="lg:mx-32 text-5xl font-bold text-blue-400">Convert Your Currencies Today</h1>
+        <p className="lg:mx-32 opacity-40 py-6">This application allows you to easily convert Currencies based on the latest exchange rates.  </p>
+     </div> 
+   )
+ }
+ 
